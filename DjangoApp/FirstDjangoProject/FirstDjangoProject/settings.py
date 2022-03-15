644ed55922e2f5e154b506e8b6b89622d5be58b6
Rengin <rengineker@gmail.com>
2022-03-15 19:11:15 +0300

    changing db to postgresql

--- DjangoApp/FirstDjangoProject/FirstDjangoProject/settings.py
+++ DjangoApp/FirstDjangoProject/FirstDjangoProject/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'FirstDjangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ProjectDB', 
+        'USER': 'postgres', 
+        'PASSWORD': 'WakeUpLove03.55',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
+
 }
 
 
